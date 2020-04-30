3a01f577390551045fb4df733af0dd9cb00a1d75
Philip Sada <sadaphillip@gmail.com>
2020-04-30 19:23:18 +0200

    added more media queries

--- resources/js/components/NavBar.js
+++ resources/js/components/NavBar.js
@@ -15,10 +15,10 @@ const NavBar = ()=>(
                     {/* <Promo /> */}
                     <nav className="navBar">
                     
-                        <div className="navMenu" onClick={handleSideBar}><IoIosMenu className="ioMenu"/>MENU</div>
+                        <div className="navMenu" onClick={handleSideBar}><IoIosMenu className="ioMenu"/><span>MENU</span></div>
                         <div className="navLogo"><a href="/" className="linkLogo">FRESHMANN</a></div>
                         <div className="navIcons">
-                            <a href="/cart" className="navCart"><AiOutlineShopping className="aiCart"/> BAG({cartItems}) </a>
+                            <a href="/cart" className="navCart"><AiOutlineShopping className="aiCart"/><span>BAG</span>({cartItems})</a>
                         </div>
                     </nav>
                 </div>
